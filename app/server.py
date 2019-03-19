28771c67831ec38aac1d6e9daf851ddbe8c89fa8
Robert Seidl <r.seidl@scisports.com>
2019-03-19 11:19:41 +0100

    update app for ball identification

--- app/server.py
+++ app/server.py
@@ -10,10 +10,12 @@ from fastai.vision import *
 
 # export_file_url = 'https://www.dropbox.com/s/v6cuuvddq73d1e0/export.pkl?raw=1'
 # export_file_url = 'https://www.dropbox.com/s/6bgq8t6yextloqp/export.pkl?raw=1'
-export_file_url = 'https://drive.google.com/uc?export=download&id=1ou6m5GvS2zo6jxjdKzCIFRqpoON8ytn6'
-export_file_name = 'export.pkl'
+# export_file_url = 'https://drive.google.com/uc?export=download&id=1ou6m5GvS2zo6jxjdKzCIFRqpoON8ytn6'
+export_file_url = 'https://drive.google.com/open?id=1UxVWtQ1x_j01TK715Xv8MJeKow_t50gp'
+export_file_name = 'export_balls.pkl'
 
-classes = ['black', 'grizzly', 'teddys']
+# classes = ['black', 'grizzly', 'teddys']
+classes = ['baseball', 'basketball', 'football', 'fussball', 'tennisball']
 path = Path(__file__).parent
 
 app = Starlette()
